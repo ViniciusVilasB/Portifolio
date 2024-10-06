f68abf7850b715acc6d04b7bca489d2516272576
Vinicius Vilas Boas <boas.vini@gmail.com>
2024-10-06 15:33:14 -0300

    Problemas contact resolvidos

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import emailjs from 'emailjs-com';
 import imgChatting from '../assets/chatting.svg';
+// import emailjs from 'emailjs-com';
 
 export default function Contact() {
 
@@ -15,16 +15,16 @@ export default function Contact() {
         setFormData({ ...formData, [name]: value });
     };
 
-    const sendEmail = (e) => {
-        e.preventDefault();
-        emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', formData, 'YOUR_USER_ID') // ATERAR PARA sistema DE EMAIL FUNCIONAR
-            .then((response) => {
-                alert('Email enviado com sucesso!');
-            }, (err) => {
-                // alert('Falha ao enviar o email. Tente novamente.');
-                alert('Essa função está em manutenção, utilize outrtos meios de comunicação.');
-            });
-    };
+    // const sendEmail = (e) => {
+    //     e.preventDefault();
+    //     emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', formData, 'YOUR_USER_ID') // ATERAR PARA sistema DE EMAIL FUNCIONAR
+    //         .then((response) => {
+    //             alert('Email enviado com sucesso!');
+    //         }, (err) => {
+    //             // alert('Falha ao enviar o email. Tente novamente.');
+    //             alert('Essa função está em manutenção, utilize outrtos meios de comunicação.');
+    //         });
+    // };
 
     return (
         <div className="min-h-screen flex items-center flex-col justify-evenly">
@@ -34,7 +34,7 @@ export default function Contact() {
                 <h2 className='text-xl px-1 text-minha-cor-1-light'>Você pode me enviar um email apenas preenchendo os campos abaixo</h2>
             </div>
 
-            <div className='flex justify-evenly w-full items-center flex-col-reverse md:flex-row gap-3'>
+            <div className='flex justify-evenly w-full items-center flex-col-reverse md:flex-row gap-3 m-10'>
                 <img
                     src={imgChatting}
                     alt="Imagem de pessoas conversando por texto"
@@ -44,7 +44,9 @@ export default function Contact() {
                     <div className="w-1 h-1/2 bg-minha-cor-2"></div>
                 </div>
 
-                <form onSubmit={sendEmail} className="w-96 border border-minha-cor-1 rounded-lg">
+                <form 
+                // onSubmit={sendEmail} 
+                className="w-96 border border-minha-cor-1 rounded-lg">
                     <div className="flex justify-center my-4 gap-6 w-full">
                         <textarea
                             name="name"
